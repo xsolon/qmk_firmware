681a1a12c857bb7c0bf71d8f7b3032c0d89c591a
Martin <martin@xsolon.net>
2017-10-31 15:09:31 -0400

    avoid alt override on layer 1

--- keyboards/ergodox_ez/keymaps/xsolon/keymap.c
+++ keyboards/ergodox_ez/keymaps/xsolon/keymap.c
@@ -67,17 +67,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 					  // left hand
 		KC_ESC,         KC_1,         KC_2,   		KC_3,   KC_4,   KC_5,   LCTL(KC_X),
 		KC_TAB,         KC_Q,         KC_W,   		KC_E,   KC_R,   KC_T,   LCTL(KC_V),
-		LT(MDIA,KC_TAB),KC_A,	      KC_S,	   	KC_D,   KC_F,   KC_G,
+		LT(MDIA,KC_TAB),KC_A,	      KC_S,	   		KC_D,   KC_F,   KC_G,
 		KC_LSFT,        KC_Z,	      KC_X,   		KC_C,   KC_V,   KC_B,   LCTL(KC_C),
 		KC_LCTL,	KC_LGUI,      KC_LALT,MO(SYMB),KC_LALT,
 		KC_ESC,  KC_DELT,
 		KC_HOME,
 		LT(SYMB,KC_SPC),LT(MDIA,KC_BSPC),LT(VSFN,KC_END),
+
 		// right hand
-		KC_MINS,		KC_6,	KC_7,   KC_8,   KC_9,   KC_0,		KC_BSPC,
-		LCTL(KC_Z),    	KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-		KC_H,   KC_J,   KC_K,   KC_L,   LT(VSFN, KC_SCLN),GUI_T(KC_QUOT),
-		LCTL(KC_Y),	KC_N, KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+		KC_MINS,		KC_6,		KC_7,		KC_8,   KC_9,			KC_0,				KC_BSPC,
+		LCTL(KC_Z),    	KC_Y,		KC_U,		KC_I,   KC_O,			KC_P,				KC_BSLS,
+						KC_H,		KC_J,		KC_K,   KC_L,			LT(VSFN, KC_SCLN),	GUI_T(KC_QUOT),
+		LCTL(KC_Y),		KC_N, KC_M, KC_COMM,	KC_DOT, CTL_T(KC_SLSH), KC_RSFT,
 		KC_LALT, KC_LEFT,KC_DOWN,KC_UP, KC_RGHT,
 		ALT_T(KC_APP),        CTL_T(KC_ESC),
 		KC_PGUP,
@@ -93,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* | Shf+F6 | End  | Back | Del  |Enter | Esc   |------|           |------| Left | Down |  Up  |Right |   +  |        |
 	* |--------+------+------+------+------+------ | Ctrl+|           |      |------+------+------+------+------+------- |
 	* |		   |   %  |   ^  |Close |   _  |   ~   |   \  |           |      | PDown| App  |   -  |   3  |   \  |        |
-	* `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-	*   |      |  F5  | F10 |  F11 |   F12 |                                       |      |    . |   0  |   =  |      |
+	* `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+	*   |      |  F5  | F10  |  F11 |       |                                       |      |    . |   0  |   =  |      |
 	*   `-----------------------------------'                                       `----------------------------------'
 	*                                        ,-------------.       ,-------------.
 	*                                        |Animat| Bri- |       |Toggle|Solid |
@@ -114,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS,			KC_F5,		KC_F10,		KC_F11, KC_F12,
 											RGB_MOD, RGB_VAD,
 													 RGB_VAI,
-								   KC_TRNS, EPRM, KC_TRNS,
+									  KC_TRNS, EPRM, KC_TRNS,
 		// right hand
 		KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
 		KC_TRNS, KC_PGUP, KC_HOME, KC_END, KC_EQL, KC_ASTR, KC_F12,
