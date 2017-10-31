dc604c164ec545e00280515f376c13d87bbc7538
Martin <martin@xsolon.net>
2017-10-31 13:55:02 -0400

    6 switch to right, vs commands on L1

--- keyboards/ergodox_ez/keymaps/xsolon/keymap.c
+++ keyboards/ergodox_ez/keymaps/xsolon/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	/* Keymap 0: Basic layer
 	*
 	* ,--------------------------------------------------.           ,--------------------------------------------------.
-	* | Esc    |   1  |   2  |   3  |   4  |   5  | Cut  |           |   6  |   7  |   8  |   9  |   0  |   -  | BackS  |
+	* | Esc    |   1  |   2  |   3  |   4  |   5  | Cut  |           |  -   |   6  |   7  |   8  |   9  |   0  | BackS  |
 	* |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
 	* | Tab    |   Q  |   W  |   E  |   R  |   T  | Paste|           | Undo |   Y  |   U  |   I  |   O  |   P  |   \    |
 	* |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
 	* `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
 	*   |Ctrl  | Win  |Alt	  | L1   | Alt  |                         		|  Alt | Left |Down  | Up   | Right  |
-	*   `----------------------------------'                                       `----------------------------------'
+	*   `----------------------------------'                                `----------------------------------'
 	*                                        ,-------------.       ,---------------.
 	*                                        | Esc  | Del  |       | App  |Ctrl/Esc|
 	*                                 ,------|------|------|       |------+--------+------.
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_HOME,
 		LT(SYMB,KC_SPC),LT(MDIA,KC_BSPC),LT(VSFN,KC_END),
 		// right hand
-		KC_6,   		KC_7,   KC_8,   KC_9,   KC_0,	KC_MINS,	KC_BSPC,
+		KC_MINS,		KC_6,	KC_7,   KC_8,   KC_9,   KC_0,		KC_BSPC,
 		LCTL(KC_Z),    	KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
 		KC_H,   KC_J,   KC_K,   KC_L,   LT(VSFN, KC_SCLN),GUI_T(KC_QUOT),
 		LCTL(KC_Y),	KC_N, KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
@@ -86,40 +86,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	/* Keymap 1: Symbol Layer
 	*
 	* ,---------------------------------------------------.           ,--------------------------------------------------.
-	* |Version  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
-	* |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
-	* |        |   {  |   }  |  [   |   ]  |  |    |	  |           |      | P Up | Home | End  |  =   |   *  |   F12  |
+	* |Version |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+	* |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
+	* | Shf+F5 |   {  |   }  |  [   |   ]  |  |    |	  |           |      | P Up | Home | End  |  =   |   *  |   F12  |
 	* |--------+------+------+------+------+-------|      |           |      |------+------+------+------+------+--------|
-	* |        | End  | Back | Del  |Enter | Esc   |------|           |------| Left | Down |  Up  |Right |   +  |        |
-	* |--------+------+------+------+------+------ |      |           |      |------+------+------+------+------+------- |
-	* |        |   %  |   ^  |Close |   _  |   ~   |      |           |      | PDown| App  |   2  |   3  |   \  |        |
+	* | Shf+F6 | End  | Back | Del  |Enter | Esc   |------|           |------| Left | Down |  Up  |Right |   +  |        |
+	* |--------+------+------+------+------+------ | Ctrl+|           |      |------+------+------+------+------+------- |
+	* |		   |   %  |   ^  |Close |   _  |   ~   |   \  |           |      | PDown| App  |   -  |   3  |   \  |        |
 	* `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-	*   | EPRM  |      |      |      |      |                                       |      |    . |   0  |   =  |      |
+	*   |      |  F5  | F10 |  F11 |   F12 |                                       |      |    . |   0  |   =  |      |
 	*   `-----------------------------------'                                       `----------------------------------'
 	*                                        ,-------------.       ,-------------.
-	*                                        |Animat|      |       |Toggle|Solid |
+	*                                        |Animat| Bri- |       |Toggle|Solid |
 	*                                 ,------|------|------|       |------+------+------.
-	*                                 |Bright|Bright|      |       |      |Hue-  |Hue+  |
-	*                                 |ness- |ness+ |------|       |------|      |      |
+	*                                 |		 |      | Bri+ |       |      |Hue-  |Hue+  |
+	*                                 |      | EPRM |------|       |------|      |      |
 	*                                 |      |      |      |       |      |      |      |
 	*                                 `--------------------'       `--------------------'
 	*/
 	// SYMBOLS
 	[SYMB] = LAYOUT_ergodox(
 		// left hand
-		VRSN, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRNS,
-		KC_TRNS, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_PIPE, KC_TRNS,
-		KC_TRNS, KC_HOME, KC_BSPC, KC_DELT, KC_ENT, KC_ESC,
-		KC_TRNS, KC_PERC, KC_CIRC, LCTL(KC_F4), LSFT(KC_MINS), KC_TILD, KC_TRNS,
-			EPRM, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-											RGB_MOD, KC_TRNS,
-													 KC_TRNS,
-								   RGB_VAD, RGB_VAI, KC_TRNS,
+		VRSN,				KC_F1,		KC_F2,		KC_F3,			KC_F4,			KC_F5,		KC_TRNS,
+		LSFT(KC_F5),		KC_LCBR,	KC_RCBR,	KC_LBRC,		KC_RBRC,		KC_PIPE,	KC_TRNS,
+		LSFT(KC_F6),		KC_HOME,	KC_BSPC,	KC_DELT,		KC_ENT,			KC_ESC,
+		KC_TRNS,			KC_PERC,	KC_CIRC,	LCTL(KC_F4),	LSFT(KC_MINS),	KC_TILD,	LCTL(KC_BSLS),
+		KC_TRNS,			KC_F5,		KC_F10,		KC_F11, KC_F12,
+											RGB_MOD, RGB_VAD,
+													 RGB_VAI,
+								   KC_TRNS, EPRM, KC_TRNS,
 		// right hand
 		KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
 		KC_TRNS, KC_PGUP, KC_HOME, KC_END, KC_EQL, KC_ASTR, KC_F12,
 				 KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_PLUS, KC_TRNS,
-		KC_TRNS, KC_PGDN, ALT_T(KC_APP), KC_2, KC_3, KC_BSLS, KC_TRNS,
+		KC_TRNS, KC_PGDN, ALT_T(KC_APP), KC_MINS, KC_3, KC_BSLS, KC_TRNS,
 		KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
 		RGB_TOG, RGB_SLD,
 		KC_TRNS,
@@ -346,9 +346,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		}
 		return false;
 		break;
-		}
-	return true;
 	}
+	return true;
+}
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
